557e40afe5bb36061ce57ebf49719401ef1675aa
Felipe do Prado Oliveira <felipedoprado1@gmail.com>
2026-09-09 17:05:35 -0300

    aula1

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,61 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  <div className="cont-app">
+
+  <h1>Trampando com React</h1>
+   <h2>Onde posso trampar como Dev?</h2>
+    <p>Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</p>
+     <p>Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</p>
+
+     <p>Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.</p>
+
+     <p>Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</p>
+
+     <p>Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.</p>
+
+     <p>Freelance e Empreendedorismo: Atuação autônoma criando soluções web para pequenos e médios negócios locais.</p>
+
+     <p>Cargos de Entrada Comuns</p>
+      
+      <p>Desenvolvedor Front-End Junior: Focado em criar interfaces funcionais e responsivas (HTML, CSS, JavaScript).</p>
+
+      <p>Desenvolvedor Full-Stack Junior / Trainee: Atuando tanto na interface quanto na integração com bancos de dados e BaaS (como Supabase ou Firebase).</p>
+
+      <p>Desenvolvedor Full-Stack Junior / Trainee: Atuando tanto na interface quanto na integração com bancos de dados e BaaS (como Supabase ou Firebase).</p>
+
+      <p>Assistente / Analista de Suporte a Software: Posição inicial estratégica para entender a regra de negócio da empresa enquanto realiza pequenas manutenções no código.</p>
+      
+      <p>Como este é um campo amplo e com diferentes trilhas de carreira, você gostaria de sugestões sobre como orientá-los para processos seletivos (como montagem de portfólio no GitHub) ou prefere focar em quais linguagens/frameworks complementares priorizar no mercado atual? Essa resposta ajudará a preparar o próximo passo da sua turma!</p>
+
+      <h3>trampo com tecnologia mas sem programação</h3>
+
+      <p>Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.</p>
+
+      <p>
+  <ul>
+
+    <li>Product Management & Negócios</li>
+    <li>Design e Experiência do Usuário (UI/UX)</li>
+    <li>Gestão de Projetos e Agilidade</li>
+    <li>Dados e Inteligência de Negócio</li>
+    <li>Vendas Técnicas e Sucesso do Cliente
+
+</li>
+
+
+  </ul>
+
+      </p>
+  </div>
+  
+  
+   
+  )
+}
+
+export default App
